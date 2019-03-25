dd44c8f0e210e4fc168b41f5ff183b85b975e712
Harris Beg <legostarwarsguy02@gmail.com>
2019-03-25 12:25:28 -0700

    Added toString method

--- Unit014/src/MatrixCount1Runner.java
+++ Unit014/src/MatrixCount1Runner.java
@@ -8,6 +8,7 @@ public class MatrixCount1Runner
 	{
 		//add code
 		MatrixCount1 m = new MatrixCount1();
+		System.out.println(m);
 		System.out.println("7: "+ m.count(7));
 		System.out.println("8: "+ m.count(8));
 		System.out.println("6: "+ m.count(6));
